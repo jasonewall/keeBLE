4624ed706c7b9f3081dde52f5128dffea68f1a52
Jason Wall <javajo@gmail.com>
2023-09-02 01:54:21 +0000

    serial example from nrf-hal repo

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,62 @@
 #![no_std]
 #![no_main]
 
-extern crate cortex_m_rt as rt;
-extern crate panic_halt;
+use panic_semihosting as _;
 
-use rt::entry;
+use cortex_m_rt::entry;
+use nrf52840_hal::clocks::Clocks;
+use nrf52840_hal::usbd::{UsbPeripheral, Usbd};
+use usb_device::class_prelude::{UsbBusAllocator, UsbBus};
+use usb_device::device::{UsbDeviceBuilder, UsbVidPid};
+use usbd_serial::{SerialPort, USB_CLASS_CDC};
+
+
 
 #[entry]
 fn main() -> ! {
-    loop {}
+    let periph = nrf52840_hal::pac::Peripherals::take().unwrap();
+    let clocks = Clocks::new(periph.CLOCK);
+    let clocks = clocks.enable_ext_hfosc();
+
+    let usb_bus = UsbBusAllocator::new(Usbd::new(UsbPeripheral::new(periph.USBD, &clocks)));
+    let mut serial = SerialPort::new(&usb_bus);
+
+    let mut usb_dev = UsbDeviceBuilder::new(&usb_bus, UsbVidPid(0x16c0, 0x27dd))
+        .manufacturer("@thejayvm Solutions")
+        .product("keeBLE Adapter Debug Serial port")
+        .serial_number("nice!nano")
+        .device_class(USB_CLASS_CDC)
+        .max_packet_size_0(64)
+        .build();
+
+    loop {
+        if !usb_dev.poll(&mut [&mut serial]) {
+            continue;
+        }
+
+        let mut buf = [0u8; 64];
+
+        match serial.read(&mut buf) {
+            Ok(count) if count > 0 => {
+                // Echo back in upper case
+                for c in buf[0..count].iter_mut() {
+                    if 0x61 <= *c && *c <= 0x7a {
+                        *c &= !0x20;
+                    }
+                }
+
+                let mut write_offset = 0;
+                while write_offset < count {
+                    match serial.write(&buf[write_offset..count]) {
+                        Ok(len) if len > 0 => {
+                            write_offset += len;
+                        }
+                        _ => {}
+                    }
+                }
+            }
+            _ => {}
+        }
+    }
+
 }
